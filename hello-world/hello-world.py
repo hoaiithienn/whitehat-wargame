b4eb3840dccaad4bf951e28c0371cbad60f65c6a
basilia <basilia@basilio.basilia>
2021-06-04 14:57:04 +0700

    Edit hello_world

--- hello-world/hello-world.py
+++ hello-world/hello-world.py
@@ -5,4 +5,5 @@ flag = flag.encode('utf-8')
 
 h = hashlib.sha1(flag)
 
-print(f'sha1({flag}) \n = {h.hexdigest() }')
+print('Flag = w31l_c0m3_t0_w4rg4m3_2O21')
+print(f'SHA1 of flag = {h.hexdigest()}')
